1cf2b4f7c54c9722037f1c79332acfd33aa87c7a
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-11 18:12:33 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+graph = {}
+g_rev = {}
+
+with open("SCC.txt") as file:
+  for line in file:
+    tail, head = map(int, line.strip().split())
+
+    if tail != head:
+      if tail not in graph:
+        graph[tail] = []
+      graph[tail].append(head)
+
+      if head not in g_rev:
+        g_rev[head] = []
+      g_rev[head].append(tail)
+
+order = []
+explore = set()
+
+for start in g_rev:
+  if start not in explore:
+    stack = [start]
+    explore.add(start)
+
+    while stack:
+      node = stack[-1]
+
+      if node in g_rev:
+        for neighbor in g_rev[node]:
+          if neighbor not in explore:
+            explore.add(neighbor)
+            stack.append(neighbor)
+
+      if node == stack[-1]:
+        order.append(stack.pop())
+
+scc = {}
+explore = set()
+
+for lead in order[::-1]:
+  if lead not in explore:
+    stack = [lead]
+    explore.add(lead)
+
+    while stack:
+      node = stack.pop()
+      explore.add(node)
+
+      if node in graph:
+        for neighbor in graph[node]:
+          if neighbor not in explore:
+            explore.add(neighbor)
+            stack.append(neighbor)
+
+      if node != lead:
+        if lead not in scc:
+          scc[lead] = []
+        scc[lead].append(node)
